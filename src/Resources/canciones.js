78ae0c4631f56827c5bb59b733941eabc74ec9c3
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-04-01 18:57:05 -0300

    Initial commmit

---
+++ src/Resources/canciones.js
@@ -0,0 +1,14 @@
+const songs = [
+    { "id": 1, "category": "game", "name": "Mario Castle", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3" },
+    { "id": 2, "category": "game", "name": "Mario Star", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/hurry-starman.mp3" },
+    { "id": 3, "category": "game", "name": "Mario Overworld", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/overworld.mp3" },
+    { "id": 4, "category": "game", "name": "Mario Stage 1", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/stage1.mp3" },
+    { "id": 5, "category": "game", "name": "Mario Stage 2", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/stage2.mp3" },
+    { "id": 6, "category": "game", "name": "Mario Star", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/starman.mp3" },
+    { "id": 7, "category": "game", "name": "Mario Underworld", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/underworld.mp3" },
+    { "id": 8, "category": "game", "name": "Mario Underwater", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/underwater.mp3" },
+    { "id": 9, "category": "game", "name": "Zelda Castle", "url": "https://assets.breatheco.de/apis/sound/files/videogame/songs/zelda_castle.mp3" },
+    { "id": 10, "category": "game", "name": "Zelda Outworld", "url": "https://assets.breatheco.de/apis/sound/files/videogame/songs/zelda_outworld.mp3" }
+];
+
+export default songs;
