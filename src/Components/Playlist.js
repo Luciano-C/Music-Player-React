a6ed975900eb1eb661b6cea0b8f831cd90868e36
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-04-02 19:04:43 -0300

    Se utilizará objeto Audio para reproducir canciones. Se escribe código general para uso

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -6,22 +6,38 @@ import "../App.css";
 
 const Playlist = () => {
 
-    let songList = songs.map((x, i) => <li key={i}><span className="songNumber">{i < 9 ? `0${i+1}` : `${i+1}`}</span><span className="songName">{x.name}</span></li>);
+    let songList = songs.map((x, i) => <li key={i}><span className="songNumber">{i < 9 ? `0${i + 1}` : `${i + 1}`}</span><span className="songName">{x.name}</span></li>);
+    
+
+    let music = new Audio("https://assets.breatheco.de/apis/sound/files/mario/songs/hurry-starman.mp3");
     
     
+    
+    const playMusic = () => {
+        
+        music.play();
+    }
+
+    const pauseMusic = () => {
+       
+        music.pause()
+        music.currentTime = 0;
+    }
+
+ 
+
     return (
         <div className="musicPlayer">
             <ul className="playlist">
                 {songList}
             </ul>
             <div className="controls">
-            <i class="fas fa-caret-square-left"></i>
-            <i class="fas fa-play"></i>
-            {/* <i class="fas fa-pause-circle"></i> */}
-            <i class="fas fa-caret-square-right"></i>
-            
-
+                <i class="fas fa-caret-square-left"></i>
+                <i class="fas fa-play" onClick={() => {playMusic()}}></i>
+                {/* <i class="fas fa-pause-circle"></i> */}
+                <i class="fas fa-caret-square-right" onClick={() => {pauseMusic()}}></i>
             </div>
+
         </div>
     )
 }
