eb8a3b73cf4f15dc4dfde5b2a145052bba3d68bb
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-04-04 15:56:43 -0400

    Funcionan el botón play/pause, pero canciones no se reproducen al hacerles click

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,42 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import songs from "../Resources/songs";
 import logo from "../logo.JPG"
 
 /* [{ "id": 1, "category": "game", "name": "Mario Castle", "url": "https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3" }] */
 
 const Playlist = () => {
+    const [currentSong, setCurrentSong] = useState({});
+    const [audio, setAudio] = useState(new Audio());
+    const [isSongPlaying, setIsSongPlaying] = useState(false);
+   
+    audio.onended = () => {
+        stopMusic();
+    };  
+    let songListHTML = songs.map((x, i) => <li key={i} onClick={() => {selectSongObject(x.name)}}><span className="songNumber">{i < 9 ? `0${i + 1}` : `${i + 1}`}</span><span className="songName">{x.name}</span></li>);
 
-    let songList = songs.map((x, i) => <li key={i}><span className="songNumber">{i < 9 ? `0${i + 1}` : `${i + 1}`}</span><span className="songName">{x.name}</span></li>);
-    
+    useEffect(() => { 
+        setAudio(new Audio(currentSong.url));
+    }, [currentSong])
 
-    let music = new Audio("https://assets.breatheco.de/apis/sound/files/mario/songs/hurry-starman.mp3");
-    
-    
-    
+  
+
+    const selectSongObject = (requiredSong) => {
+        let songObject = songs.filter(x => x.name === requiredSong).reduce(x => x);
+        setCurrentSong(songObject);
+    }
+
+
     const playMusic = () => {
-        
-        music.play();
+        audio.play();
+        setIsSongPlaying(true);
+ 
     }
 
     const pauseMusic = () => {
-       
-        music.pause()
-        music.currentTime = 0;
+        audio.pause();
+        setIsSongPlaying(false);
+
     }
 
- 
+   const stopMusic = () => {
+       audio.pause();
+       audio.currentTime = 0;
+       setIsSongPlaying(false);
+   }
+
+
 
     return (
         <div className="musicPlayer">
-            <img id="logo" src={logo}/>
+            <img id="logo" src={logo} alt="sloth logo"/>
             <ul className="playlist">
-                {songList}
+                {songListHTML}
             </ul>
             <div className="controls">
                 <i className="fas fa-caret-square-left"></i>
-                <i className="fas fa-play" onClick={() => {playMusic()}}></i>
-                {/* <i class="fas fa-pause-circle"></i> */}
-                <i className="fas fa-caret-square-right" onClick={() => {pauseMusic()}}></i>
+                {isSongPlaying === true ? <i className="fas fa-pause-circle" onClick={() => {pauseMusic()}}></i> : <i className="fas fa-play" onClick={() => {playMusic()}}></i> }
+                    
+                <i className="fas fa-caret-square-right" onClick={() => console.log("hola")}></i>
             </div>
 
         </div>
