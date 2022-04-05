38a6fcd0179bb35075a1cff6bbcd2ec2c84b7390
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-04-04 21:06:45 -0400

    Botones next song y prev song funcionando

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -8,24 +8,22 @@ const Playlist = () => {
     const [currentSong, setCurrentSong] = useState({});
     const [audio] = useState(new Audio("https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3"));
     const [isSongPlaying, setIsSongPlaying] = useState(false);
-   
+
     audio.onended = () => {
         stopMusic();
-    };  
-    
-    let songListHTML = songs.map((x, i) => <li key={i} onClick={() => {clickHandler(i)}}><span className="songNumber">{i < 9 ? `0${i + 1}` : `${i + 1}`}</span><span className="songName">{x.name}</span></li>);
+    };
+
+    let songListHTML = songs.map((x, i) => <li key={i} onClick={() => { clickHandler(i) }}><span className="songNumber">{i < 9 ? `0${i + 1}` : `${i + 1}`}</span><span className="songName">{x.name}</span></li>);
 
     useEffect(() => {
         pauseMusic();
         audio.src = currentSong.url;
-        console.log(audio.src);
         playMusic();
     }, [currentSong]);
 
-  
+
     const clickHandler = (i) => {
         setCurrentSong(songs[i]);
-        console.log(isSongPlaying);      
     }
 
 
@@ -38,30 +36,38 @@ const Playlist = () => {
     const pauseMusic = () => {
         audio.pause();
         setIsSongPlaying(false);
-    }
+    };
 
-   const stopMusic = () => {
-       audio.pause();
-       audio.currentTime = 0;
-       setIsSongPlaying(false);
-   }
+    const stopMusic = () => {
+        audio.pause();
+        audio.currentTime = 0;
+        setIsSongPlaying(false);
+    };
+
+    const nextSong = () => {
+        let currentIndex = songs.indexOf(currentSong);
+        currentIndex < songs.length -1 ? setCurrentSong(songs[currentIndex + 1]) : setCurrentSong(songs[0]);
+    };
+
+    const prevSong = () => {
+        let currentIndex = songs.indexOf(currentSong);
+        currentIndex === 0 ? setCurrentSong(songs[songs.length - 1]) : setCurrentSong(songs[currentIndex - 1]);
+    }
 
 
 
     return (
         <div className="musicPlayer">
-            <img id="logo" src={logo} alt="sloth logo"/>
+            <img id="logo" src={logo} alt="sloth logo" />
             <ul className="playlist">
                 {songListHTML}
             </ul>
             <div className="controls">
-                <i className="fas fa-caret-square-left"></i>
-                {isSongPlaying === true ? <i className="fas fa-pause-circle" onClick={() => {pauseMusic()}}></i> : <i className="fas fa-play" onClick={() => {playMusic()}}></i> }
-                    
-                <i className="fas fa-caret-square-right" onClick={() => console.log("hola")}></i>
-            </div>
-            
+                <i className="fas fa-caret-square-left" onClick={() => {prevSong()}}></i>
+                {isSongPlaying === true ? <i className="fas fa-pause-circle" onClick={() => { pauseMusic() }}></i> : <i className="fas fa-play" onClick={() => { playMusic() }}></i>}
 
+                <i className="fas fa-caret-square-right" onClick={() => nextSong()}></i>
+            </div>
         </div>
     )
 }
